62200f441ad4a18a2735151b92d75ddd31921033
loragonzaleze <61980534+loragonzaleze@users.noreply.github.com>
2021-12-11 02:38:00 -0500

    Added login UI

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,8 +1,26 @@
 import axios from "axios";
 import React from "react";
-class Login extends React.Component {
-    state = {
 
+
+interface LoginProps {}
+interface LoginState {
+    username : string,
+    password : string
+}
+    
+
+class Login extends React.Component<LoginProps, LoginState> {
+    constructor(props : any){
+        super(props)
+        this.state = {
+            username : '',
+            password : ''
+        }
+
+        this.handleUsernameSubmit = this.handleUsernameSubmit.bind(this)
+        this.handleUsernameChange = this.handleUsernameChange.bind(this)
+        this.handlePasswordChange = this.handlePasswordChange.bind(this)
+        
     }
     componentDidMount(){
         axios.get("https://dev-gatormotorsportsapi.herokuapp.com/mongodb").then(res =>{
@@ -14,10 +32,42 @@ class Login extends React.Component {
             console.log(error)
         })
     }
+
+    handleUsernameChange(event){
+        this.setState({username : event.target.value})
+    }
+
+    handlePasswordChange(event){
+        this.setState({password : event.target.value})
+    }
+
+    handleUsernameSubmit(event){
+        console.log("Username was submitted")
+        console.log(this.state.username)
+        console.log("Password submitted")
+        console.log(this.state.password)
+        event.preventDefault()
+    }
     render() {
         return (
             <div>
-                <h1>THIS WILL BE THE LOGIN PAGE</h1>
+                <h1>Login</h1>
+                <form onSubmit={this.handleUsernameSubmit}>
+
+                    <div>
+                        <label>
+                            Username:
+                            <input type="text" value={this.state.username} onChange={this.handleUsernameChange}/>
+                        </label>
+                    </div>
+                    <div>
+                        <label>
+                            Password: 
+                            <input type="password" value={this.state.password} onChange={this.handlePasswordChange}/>
+                        </label>
+                    </div>
+                    <input type="submit" value="Submit"/>
+                </form>
             </div>
         )
     }
