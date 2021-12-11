1bdc194d7a0ad14ef109620a1b8b560ae98e75fa
loragonzaleze <61980534+loragonzaleze@users.noreply.github.com>
2021-12-10 23:39:42 -0500

    Removed backend, in seperate folders

--- backend/api/TestApiHandler.py
+++
@@ -1,7 +0,0 @@
-from flask_restful import Api, Resource, reqparse
-
-class TestApiHandler(Resource):
-    def get(self):
-        return {
-            'message' : 'THis shoudl display new values'
-        }
